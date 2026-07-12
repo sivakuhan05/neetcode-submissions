e8e8475d60231b6ca7ddb0241166a07debcf9a12
sivakuhan05 <129924698+sivakuhan05@users.noreply.github.com>
2026-07-12 18:31:17 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash = {}
+
+        array_length = len(strs)
+
+        for i in range(array_length):
+            count = [0 for _ in range(26)]
+            
+            for j in strs[i]:
+                count[ord(j) - ord('a')] += 1 
+            
+            key = tuple(count)
+
+            if key in hash:
+                hash[key].append(strs[i])
+            else:
+                hash[key] = [strs[i]]
+
+        return list(hash.values())
